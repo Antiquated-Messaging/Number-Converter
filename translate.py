f9e25b9aef9aa0f8beb1979aec5cf4238ba1d77b
Matthew <mleone18@choate.edu>
2017-12-07 14:24:44 -0500

    Number code

--- translate.py
+++ translate.py
@@ -7,7 +7,7 @@ class Numcode:
 		self.numb=['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26']
 		for x in range(len(self.message)):
            	if self.message[x].isalpha(): 
-                self.numb[x]=self.alpha[x]
+            	self.numb[x]=self.alpha[x]
 				return self.translation.append(self.numb)
 			else:
 				if self.message[x] == '.':
